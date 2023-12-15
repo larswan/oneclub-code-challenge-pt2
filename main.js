593018cd570b181ec6bcd8b0347fc68f6baf0cad
Larson Collier <collierlarson@gmail.com>
2023-12-15 15:58:17 -0500

    button checks form has text on click

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './style.css'
+import { validateInput } from './modules';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const editableForm = document.getElementById('editableForm');
+  const itemNameInput = document.getElementById('itemNameInput');
+  const newItemBtn = document.getElementById('newItemBtn');
+  const itemList = document.getElementById('itemList');
+
+  newItemBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(validateInput(itemNameInput.value)){
+      // add item
+      console.log("valid")
+      editableForm.reset()
+    }
+    
+  })
+})
---
+++ modules.js
@@ -0,0 +1,6 @@
+export function validateInput (inputText){
+    console.log(inputText)
+    if (inputText) {
+        return inputText
+    } else (window.alert("Invalid entry"))
+}
